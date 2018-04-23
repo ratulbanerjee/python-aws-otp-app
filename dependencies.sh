8b57989599a08c20f0d57327f7831f722bc80024
ratul banerjee <ratulbanerjee01@gmail.com>
2018-04-23 23:05:02 +0530

    added dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -6,6 +6,6 @@ pip install awscli
 pip install django
 pip install boto3
 pip install requests
-nohup python manage.py runserver ec2-35-161-109-195.us-west-2.compute.amazonaws.com:8000 > /dev/null 2>&1 &
-echo "ending program"
+
+echo "dependencies are installed"
 
